ae654462a7c35a98f4594cf8c4984e10933947a9
jpartido_ <132030695+echOCypher@users.noreply.github.com>
2025-01-15 18:18:27 +0800

    Update Greeting.sol
    i have added a state variable 'age' for a more intuitive approach

--- Greeting.sol
+++ Greeting.sol
@@ -3,17 +3,26 @@ pragma solidity ^0.8.11;
 
 contract Greeting {
     string public name;
+    string public age;
     string public greetingPrefix = "Hello ";
 
-    constructor(string memory initialName) {
+    constructor(string memory initialName, string memory initialAge) {
         name = initialName;
+        age = initialAge;
     }
 
     function setName(string memory newName) public {
         name = newName;
     }
+    function setAge(string memory newAge) public {
+        age = newAge;
+    }
 
     function getGreeting() public view returns(string memory) {
         return string(abi.encodePacked(greetingPrefix, name));
+    }
+
+    function getAge() public view returns(string memory) {
+        return string(age);
     }
 }
